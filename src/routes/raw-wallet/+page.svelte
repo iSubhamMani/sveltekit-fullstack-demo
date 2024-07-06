ab79377fbb7fd499f594c087d8e5db47ef345456
Subham Mani <manisubham09@gmail.com>
2024-07-06 13:38:05 +0530

    Update raw wallet button color

--- src/routes/raw-wallet/+page.svelte
+++ src/routes/raw-wallet/+page.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <section>
-        <div class="flex justify-center my-8"><button class="bg-yellow-500 text-black font-bold py-3 px-4 rounded-md" on:click={createKeys}>Generate new wallet keys</button></div>
+        <div class="flex justify-center my-8"><button class="bg-yellow-600 text-black font-bold py-3 px-4 rounded-md" on:click={createKeys}>Generate new wallet keys</button></div>
         <p class="my-6 text-lg text-center">Private key WIF format is <span class="font-bold">{WIF}</span></p>
         <p class="my-6 text-lg text-center">Your bitcoin address is <span class="font-bold">{address}</span></p>
 </section>
